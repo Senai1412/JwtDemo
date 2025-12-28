908679885e760cef80918333e74a65ff57b4370d
eileenLien <eileenlien1412@gmail.com>
2025-12-28 22:48:19 +0800

    chore: add jwt test files.

---
+++ src/main/java/com/demo/JwtDemo/Configuration/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.demo.JwtDemo.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> req.requestMatchers(WHITE_LIST_URL)
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(
+                        authenticationJwtTokenFilter(),
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
+        return http.build();
+    }
+
+    // ...
+}
---
+++ src/main/java/com/demo/JwtDemo/Filter/AuthenticationJwtTokenFilter.java
@@ -0,0 +1,6 @@
+package com.demo.JwtDemo.Filter;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class AuthenticationJwtTokenFilter extends OncePerRequestFilter {
+}
